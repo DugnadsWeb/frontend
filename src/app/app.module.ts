745420d92d374c8be33aa285106a9939ecf4d303
alex <ole.alexander89@gmail.com>
2017-02-20 05:20:00 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute, RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
+const appRoutes: Routes = [
+  //{ path: 'login', component: LoginComponent},
+  { path: '', component: AppComponent },
+  { path: '**', component: NotFoundComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    MainMenuComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgbModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
