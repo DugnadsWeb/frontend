b90ce5487d2bcfe27dd2cc948ae656171e648a56
Mortenher <Morten@Hermanseter.com>
2017-03-31 13:51:23 +0200

    profilepic on members

--- src/app/components/members-list/members-list.component.ts
+++ src/app/components/members-list/members-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserBoxComponent } from '../user-box/user-box.component';
 import { User } from '../../models/models';
+import { UserService } from '../../services/services';
 
 @Component({
   selector: 'members-list',
@@ -16,9 +17,15 @@ export class MembersListComponent implements OnInit {
   @Input()
   title:string;
 
-  constructor() { }
+  constructor(private userService : UserService) { }
 
   ngOnInit() {
+  	for(var i = 0; i < this.users.length; i++)
+  	{
+  		this.userService.getPicture(this.users[i].email).subscribe((result) => {
+  				
+  		});
+  	}
   }
 
 }
